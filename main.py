7a5a07c981b3c62b72eb1edb19181ad8fcb24306
Buco <arnaud@grimbert.com>
2022-04-05 16:12:41 +0200

    Make support link more visible because VPS sadly isn't free :(

--- main.py
+++ main.py
@@ -36,6 +36,7 @@ from routers.info import blueprint as info_blueprint
 from routers.tools import blueprint as tools_blueprint
 
 app = Quart(__name__, template_folder="static/html/")
+PAYPAL_ICON = """<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-paypal" viewBox="0 0 16 16"><path d="M14.06 3.713c.12-1.071-.093-1.832-.702-2.526C12.628.356 11.312 0 9.626 0H4.734a.7.7 0 0 0-.691.59L2.005 13.509a.42.42 0 0 0 .415.486h2.756l-.202 1.28a.628.628 0 0 0 .62.726H8.14c.429 0 .793-.31.862-.731l.025-.13.48-3.043.03-.164.001-.007a.351.351 0 0 1 .348-.297h.38c1.266 0 2.425-.256 3.345-.91.379-.27.712-.603.993-1.005a4.942 4.942 0 0 0 .88-2.195c.242-1.246.13-2.356-.57-3.154a2.687 2.687 0 0 0-.76-.59l-.094-.061ZM6.543 8.82a.695.695 0 0 1 .321-.079H8.3c2.82 0 5.027-1.144 5.672-4.456l.003-.016c.217.124.4.27.548.438.546.623.679 1.535.45 2.71-.272 1.397-.866 2.307-1.663 2.874-.802.57-1.842.815-3.043.815h-.38a.873.873 0 0 0-.863.734l-.03.164-.48 3.043-.024.13-.001.004a.352.352 0 0 1-.348.296H5.595a.106.106 0 0 1-.105-.123l.208-1.32.845-5.214Z</><svg/>"""
 app.asgi_app = ProxyHeadersMiddleware(app.asgi_app, trusted_hosts=["127.0.0.1"])
 app.config["site_description"] = (
     "An easy to use API that allows you to get waifu pictures from an archive of over 4000 images "
@@ -119,7 +120,7 @@ async def inject_global_infos():
 <a type="button" href="/tags/" style="color: #fff" class="btn btn-lg {'current' if current_path == '/tags/' else 'heffect'} text-start shadow-none"><span class="bi-tags"></span> Tags</a>
 <a type="button" href="/upload/" style="color: #fff" class="btn btn-lg {'current' if current_path == '/upload/' else 'heffect'} text-start shadow-none"><span class="bi-upload"></span> Upload</a>
 <a type="button" href="/docs/" style="color: #fff" class="btn btn-lg {'current' if current_path == '/docs/' else 'heffect'} text-start shadow-none"><span class="bi-book-half"></span> Docs</a>
-<button type="button" style="color: #fff" class="btn btn-lg {'current' if current_path in ('/support/', '/contact/') else 'heffect'} text-start shadow-none" data-bs-toggle="offcanvas" data-bs-target="#info-sidebar" aria-controls="info-sidebarlabel"><span class="bi-info-lg"></span> Info<span class="bi-arrow-return-right float-end"></span></button>
+<button type="button" style="color: #fff" class="btn btn-lg {'current' if current_path in ('/support/', '/contact/') else 'heffect'} text-start shadow-none" data-bs-toggle="offcanvas" data-bs-target="#info-sidebar" aria-controls="info-sidebarlabel">{PAYPAL_ICON} Info & Support Us<span class="bi-arrow-return-right float-end"></span></button>
 <a type="button" href="{'/logout/' if loged else '/login/'}" style="color: #fff" class="btn btn-lg heffect text-start shadow-none"><span class="{'bi-box-arrow-right' if loged else 'bi-box-arrow-in-left'}"></span> {'Logout' if loged else 'Login'}</a>
 
 """
@@ -160,10 +161,7 @@ async def inject_global_infos():
             <a type="button" class="btn btn-lg heffect text-start {'current' if current_path == '/contact/' else 'heffect'} shadow-none" href="/contact/" style="color: #fff"><span class="bi-telephone-forward"></span> Contact us</a>
             <a type="button" class="btn btn-lg heffect text-start shadow-none" href="https://github.com/Waifu-im/" style="color: #fff" ><span class="bi-github"></span> Github</a>
             <a type="button" class="btn btn-lg heffect text-start shadow-none" href="{app.config['bot_invite']}" style="color: #fff"><span class="bi-discord"></span> Discord Bot</a>
-            <a type="button" class="btn btn-lg heffect text-start shadow-none" href="https://paypal.me/arnaudbucolo" style="color: #fff"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-paypal" viewBox="0 0 16 16">
-            <path d="M14.06 3.713c.12-1.071-.093-1.832-.702-2.526C12.628.356 11.312 0 9.626 0H4.734a.7.7 0 0 0-.691.59L2.005 13.509a.42.42 0 0 0 .415.486h2.756l-.202 1.28a.628.628 0 0 0 .62.726H8.14c.429 0 .793-.31.862-.731l.025-.13.48-3.043.03-.164.001-.007a.351.351 0 0 1 .348-.297h.38c1.266 0 2.425-.256 3.345-.91.379-.27.712-.603.993-1.005a4.942 4.942 0 0 0 .88-2.195c.242-1.246.13-2.356-.57-3.154a2.687 2.687 0 0 0-.76-.59l-.094-.061ZM6.543 8.82a.695.695 0 0 1 .321-.079H8.3c2.82 0 5.027-1.144 5.672-4.456l.003-.016c.217.124.4.27.548.438.546.623.679 1.535.45 2.71-.272 1.397-.866 2.307-1.663 2.874-.802.57-1.842.815-3.043.815h-.38a.873.873 0 0 0-.863.734l-.03.164-.48 3.043-.024.13-.001.004a.352.352 0 0 1-.348.296H5.595a.106.106 0 0 1-.105-.123l.208-1.32.845-5.214Z"/>
-            </svg> Support us
-            </a>
+            <a type="button" class="btn btn-lg heffect text-start shadow-none" href="https://paypal.me/arnaudbucolo" style="color: #fff">{PAYPAL_ICON} Support us</a>
         </div>
     </div>
 </div>
