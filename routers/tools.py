9ad2c72b42ed75a613256c28edb182ceeee4e83f
Bucolo <arnaud@grimbert.com>
2022-04-20 21:01:25 +0200

    base for auth page

--- routers/tools.py
+++ routers/tools.py
@@ -78,10 +78,10 @@ async def authorize_fav():
 async def authorization_callback():
     user = await fetch_user_safe()
     infos = None
-    infos = current_app.auth_rule.loads_unsafe(request.args.get('infos'))
+    infos = current_app.auth_rule.loads_unsafe(str(request.args.get('infos')))
     infos = infos[1]
     print(str(infos))
-    if infos['temp_token'] != current_app.config["temp_auth_tokens"][user.id]:
+    if str(infos['temp_token']) != str(current_app.config["temp_auth_tokens"][user.id]):
         quart.abort(403, description="Invalid temporary token.")
     redirect_uri = urllib.parse.unquote(infos['redirect_uri']) if infos.get('redirect_uri') else None
     data = [(infos['user_id'], p, user.id) for p in infos['permissions']]
